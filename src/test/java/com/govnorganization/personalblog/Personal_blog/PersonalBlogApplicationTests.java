8a91f142676b34c850dc400fcaeda92050ee5832
Fiction <goosefromhellyes@gmail.com>
2025-12-09 21:11:38 +0100

    wrapped with backend

---
+++ src/test/java/com/govnorganization/personalblog/Personal_blog/PersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govnorganization.personalblog.Personal_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
